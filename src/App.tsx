b52ffd30b8453ce57fc09e7913f8648f2859c8fe
erishBRBS <erishberboso10@gmail.com>
2025-09-20 10:52:40 +0800

    wrapper set

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,13 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-900 text-white">
-      <div className="text-center">
-        <h1 className="text-4xl font-bold mb-4">Hello Tailwind + React + TS 🚀</h1>
-        <button
-          className="px-4 py-2 bg-blue-500 hover:bg-blue-600 rounded-lg"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
+    //Wrapper
+    <div className="bg-gray-900 text-white px-100">
+      {/*Content*/}
+      <div className="min-h-screen bg-gray-500">
       </div>
+                  <div className="min-h-screen bg-gray-600"></div>
+                  <div className="min-h-screen bg-gray-700"></div>
     </div>
   )
 }
