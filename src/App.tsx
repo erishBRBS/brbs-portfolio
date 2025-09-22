89fd432eda55b1382434a9760cbc090870064a4e
erishBRBS <erishberboso10@gmail.com>
2025-09-22 18:23:25 +0800

    sphere border remove

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ function App() {
         </div>
 
         {/* Spehere section */}
-        <div className="flex items-center justify-center h-[20%] border"> {/* Adjust the vh */}
+        <div className="flex items-center justify-center h-[20%]"> {/* Adjust the vh */}
           <Sphere />
         </div>
 
